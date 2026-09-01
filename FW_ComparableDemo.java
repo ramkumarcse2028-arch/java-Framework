9d13f0c60322059c38474ec84d394ef70b879616
ramkumarcse2028-arch <ramkumarcse2028@gmail.com>
2026-09-01 22:43:54 +0530

    Add files via upload

---
+++ FW_ComparableDemo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FW_ComparableDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add(new Student("Charlie", 3.5));
+        list.add(new Student("Bob", 3.7));
+        list.add(new Student("Alioe", 3.5));
+        list.add(new Student("Akshit", 3.9));
+        List(Intger> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(99);
+        numbers.add(null);
+
+        System.out.println(numbers);
+
+
+
+    }
+
+
+
+
+    
+}
